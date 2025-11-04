b0d76dd2718f298ac799f4f243aa4d481607f119
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2025-11-04 14:39:27 +0100

    implement flash sales countdown

--- src/component/common/FlashSales.jsx
+++ src/component/common/FlashSales.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import Countdown from "react-countdown";
+import CountDownRendered from "../../utils/CountDownRendered";
 
 const FlashSales = () => {
-  return (
-    <section className='py-15'>
-        <div className="container mx-auto">
-            <div className='flex flex-start flex-col gap-6'>
-                <div className='flex flex-start items-center gap-4'>
-                    <div className='w-6 h-8 bg-amber-500'></div>
-                    <p className='text-[16px]'>Today's </p>
+
+const targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 30); // Set target time to 30 days from now
+
+    return (
+        <section className='py-15'>
+            <div className="container mx-auto">
+                <div className='flex flex-start flex-col gap-6'>
+                    <div className='flex flex-start items-center gap-4'>
+                        <div className='w-6 h-8 bg-amber-500'></div>
+                        <p className='text-[16px]'>Today's </p>
+                    </div>
+
+                    <div className='flex flex-start items-center gap-20'>
+                        <div className='flex flex-start items-center gap-4'>
+                            <h2 className='font-bold text-5xl'>Flash Sales</h2>
+                            <div></div>
+                        </div>
+
+                        <>
+                        <Countdown date={targetDate} renderer={CountDownRendered} />
+                        </>
+
+                    </div>
                 </div>
-                <div className='flex flex-start items-center gap-4'>
-                    <h2 className='font-bold text-2xl'>Flash Sales</h2>
-                    <div></div>
+
+
+                <div>
+                    {/* Flash sales content goes here */}
+
                 </div>
             </div>
-
-
-            <div></div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default FlashSales
---
+++ src/utils/CountDownRendered.jsx
@@ -0,0 +1,46 @@
+import Countdown from "react-countdown"
+
+const CountDownRendered = ({ days, hours, minutes, seconds, completed }) => {
+
+  if (completed) {
+    // Render a completed state
+    return <span>Flash Sale has ended!</span>;
+  } else {
+
+    return (
+      <div className='flex flex-start items-center gap-4 mt-4 text-center w-max px-4 py-2'>
+        <div>
+          <p className=''>Days</p>
+          <h1 className='text-3xl font-bold'>{String(days).padStart(2, "0")}</h1>
+        </div>
+
+        <div className='font-semibold text-3xl mt-6'>:</div>
+
+        <div>
+          <p>Hours</p>
+          <h1 className='text-3xl font-bold'>{String(hours).padStart(2, "0")}</h1>
+        </div>
+
+        <div className='font-semibold text-3xl mt-6'>:</div>
+
+
+        <div>
+          <p>Minutes</p>
+          <h1 className='text-3xl font-bold'>{String(minutes).padStart(2, "0")}</h1>
+        </div>
+
+        <div className='font-semibold text-3xl mt-6'>:</div>
+
+
+        <div>
+          <p>Seconds</p>
+          <h1 className='text-3xl font-bold'>{String(seconds).padStart(2, "0")}</h1>
+        </div>
+
+
+      </div>
+    )
+  }
+}
+
+export default CountDownRendered;
